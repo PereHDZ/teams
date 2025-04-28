00b3957aea0097e4e70589e416a3279b048aee6e
PereHDZ <pere.hdz.salleras@gmail.com>
2025-04-28 19:44:14 +0200

    Optimize grid generation code

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,15 @@ function App() {
   const hisui = {name: 'Hisui', start: 899, end:905, color: '#7c530d'}
   const paldea = {name: 'Paldea', start: 906, end:1025, color: '#7c0d0d'}
 
+  const regions = [kanto, johto, hoenn, sinnoh, unova, kalos, alola, galar, hisui, paldea]
+
 
   return (
     <>
       <div className='center'>
-        <GenGrid start={kanto.start} end={kanto.end} color={kanto.color} name={kanto.name}/>
-        <GenGrid start={johto.start} end={johto.end} color={johto.color} name={johto.name}/>
-        <GenGrid start={hoenn.start} end={hoenn.end} color={hoenn.color} name={hoenn.name}/>
-        <GenGrid start={sinnoh.start} end={sinnoh.end} color={sinnoh.color} name={sinnoh.name}/>
-        <GenGrid start={unova.start} end={unova.end} color={unova.color} name={unova.name}/>
-        <GenGrid start={kalos.start} end={kalos.end} color={kalos.color} name={kalos.name}/>
-        <GenGrid start={alola.start} end={alola.end} color={alola.color} name={alola.name}/>
-        <GenGrid start={galar.start} end={galar.end} color={galar.color} name={galar.name}/>
-        <GenGrid start={hisui.start} end={hisui.end} color={hisui.color} name={hisui.name}/>
-        <GenGrid start={paldea.start} end={paldea.end} color={paldea.color} name={paldea.name}/>
+        {regions.map(region => {
+          return (<GenGrid start={region.start} end={region.end} color={region.color} name={region.name}/>)
+        })}
 
         <TeamGrid/>
       </div>     
