7a3028818c306a4a26f3e7c26c63018235ea2535
PereHDZ <pere.hdz.salleras@gmail.com>
2025-04-28 19:51:56 +0200

    Optimize grid generation code

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import GenGrid from './components/GenGrid'
 import TeamGrid from './components/TeamGrid'
 
 function App() {
-  const kanto = {name: 'Kanto', start: 1, end:151, color: '#d87d7d'}
-  const johto = {name: 'Johto', start: 152, end:251, color: '#f2d367'}
-  const hoenn = {name: 'Hoenn', start: 252, end:386, color: '#009551'}
-  const sinnoh = {name: 'Sinnoh', start: 387, end:493, color: '#72a2ce'}
-  const unova = {name: 'Unova', start: 494, end:649, color: '#a2aab3'}
-  const kalos = {name: 'Kalos', start: 650, end:721, color: '#e7bde2'}
-  const alola = {name: 'Alola', start: 722, end:809, color: '#e6b065'}
-  const galar = {name: 'Galar', start: 810, end:898, color: '#b588d6'}
-  const hisui = {name: 'Hisui', start: 899, end:905, color: '#7c530d'}
-  const paldea = {name: 'Paldea', start: 906, end:1025, color: '#7c0d0d'}
-
-  const regions = [kanto, johto, hoenn, sinnoh, unova, kalos, alola, galar, hisui, paldea]
-
+  function newRegion (name, start, end, color){
+    return {name, start, end, color}
+  }
+  
+  const regions = [
+    newRegion('Kanto', 1, 151, '#d87d7d'),
+    newRegion('Johto', 152, 251, '#f2d367'),
+    newRegion('Hoenn', 252, 386, '#009551'),
+    newRegion('Sinnoh', 387, 493, '#72a2ce'),
+    newRegion('Unova', 493, 649, '#a2aab3'),
+    newRegion('Kalos', 650, 721, '#e7bde2'),
+    newRegion('Alola', 722, 809, '#e6b065'),
+    newRegion('Galar', 810, 898, '#b588d6'),
+    newRegion('Hisui', 899, 905, '#7c530d'),
+    newRegion('Paldea', 906, 1025, '#7c0d0d')
+  ]
 
   return (
     <>
